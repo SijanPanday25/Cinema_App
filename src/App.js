c6b264ad4663792b3e55ec23969f8a9d3c4e36a1
matthewleydon <lazdich@hotmail.co.uk>
2022-03-21 22:13:51 +0000

    API set up and initial testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
+
+  // API URL
+  const API_URL = 'https://api.themoviedb.org/3'
+  // API Key
+  const API_KEY = 'a89d14b3271db6828a54ca460d859bbc'
+  // The array we will set the movies to
+  const [movies, setMovies] = useState([])
+
+  // This is the constant I have set for the API request we make when the application is opened, which grabs the movies
+  const fetchMovies = async () => {
+    const {data: {results}} = await axios.get(`${API_URL}/discover/movie`, {
+      params: {
+        api_key: API_KEY
+      }
+    })
+
+    setMovies(results)
+
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+  const renderMovies = () => (
+    movies.map(movie => (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+      />
+    ))
+  )
+
   return (
-    <div>
+    <div className= 'trailerApp'>
+      <h1>Hello World</h1>
+      <div className='container'>
+         {renderMovies()}
+         </div>
     </div>
   );
 }
