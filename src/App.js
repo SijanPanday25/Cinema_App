20817a64e29f431f1601df708a556191879aaa6f
Matthew Leydon <lazdich@hotmail.co.uk>
2022-03-22 11:05:56 +0000

    Added search and discover functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+// axios is the service i have used to carry out the API pull requests. It seems to be the most popular one to use.
 import axios from 'axios';
 import './App.css';
 import MovieCard from './components/MovieCard';
@@ -9,14 +10,19 @@ function App() {
   const API_URL = 'https://api.themoviedb.org/3'
   // API Key
   const API_KEY = 'a89d14b3271db6828a54ca460d859bbc'
-  // The array we will set the movies to
+  // The array we will set the movie list to
   const [movies, setMovies] = useState([])
+  // The search box will be set to a string
+  const [searchKey, setSearchKey] = useState('')
 
-  // This is the constant I have set for the API request we make when the application is opened, which grabs the movies
-  const fetchMovies = async () => {
-    const {data: {results}} = await axios.get(`${API_URL}/discover/movie`, {
+  // This is the constant I have set for the API request we make when the application is opened, which grabs the movies, and subsequently reacts when a search function is submitted
+  const fetchMovies = async (searchKey) => {
+    // If a search key is set, we set it to search type, otherwise, we set it to discover movies
+    const type = searchKey ? "search" : "discover"
+    const {data: {results}} = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
-        api_key: API_KEY
+        api_key: API_KEY,
+        query: searchKey
       }
     })
 
@@ -28,6 +34,7 @@ function App() {
     fetchMovies()
   }, [])
 
+  
   const renderMovies = () => (
     movies.map(movie => (
       <MovieCard
@@ -37,11 +44,30 @@ function App() {
     ))
   )
 
+  // The search movies function which uses the searchKey
+  const searchMovies = (e) => {
+    e.preventDefault()
+    fetchMovies(searchKey)
+  }
+
+
+// What's returned into the HTML
+
   return (
     <div className= 'trailerApp'>
-      <h1>Hello World</h1>
+      <header className='header'>
+        <div className='header-content'>
+          <h1>Movie App</h1>
+{/* Search button, which sets the text input to Search Key when submitted */}
+            <form onSubmit={searchMovies}>
+              <input type='text' onChange={(e) => setSearchKey(e.target.value)}/>
+              <button type='submit'>Search</button>
+            </form>
+        </div>
+      </header>
+{/* The const renderMovies which reutnrs the list of movies */}
       <div className='container'>
-         {renderMovies()}
+        {renderMovies()}
          </div>
     </div>
   );
